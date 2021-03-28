136c8e4342550306cad96cdc7923e09813ab2335
moguri846 <dkffkekahsl@naver.com>
2021-03-28 21:39:47 +0900

    add router

---
+++ client/src/routes/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import MainView from '../view/MainView.vue';
+import WorldView from '../view/WorldView.vue';
+import VaccineView from '../view/VaccineView.vue';
+import SocialDistancingView from '../view/SocialDistancingView.vue';
+import FaqView from '../view/FqaView.vue';
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: MainView,
+    },
+    {
+      path: '/world',
+      name: 'world',
+      component: WorldView,
+    },
+    {
+      path: '/vaccine',
+      name: 'vaccine',
+      component: VaccineView,
+    },
+    {
+      path: 'social-distancin',
+      name: 'social-distancin',
+      component: SocialDistancingView,
+    },
+    {
+      path: 'faq',
+      name: 'faq',
+      component: FaqView,
+    },
+  ],
+});
